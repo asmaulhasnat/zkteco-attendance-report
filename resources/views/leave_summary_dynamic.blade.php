6543b05d9168a50b63000128aafb20bd9a0211ee
asmaulhasnat <30300216+asmaulhasnat@users.noreply.github.com>
2026-06-17 12:42:34 +0600

    allow leave code show

--- resources/views/leave_summary_dynamic.blade.php
+++ resources/views/leave_summary_dynamic.blade.php
@@ -5,11 +5,11 @@
 
 <tr>
     <th colspan="3" align="left">Name: {{$employee->first_name ?? ''}} {{$employee->last_name ?? ''}}</th>
-    <th colspan="{{count($leave_codes)}}" align="left">Id No: {{$employee->emp_code ?? ''}}</th>
+    <th colspan="{{count($leave_codes)*2}}" align="left">Id No: {{$employee->emp_code ?? ''}}</th>
 </tr>
 <tr>
     <th colspan="3" align="left">Designation: {{$employee->position_name  ?? ''}}</th>
-    <th colspan="{{count($leave_codes)}}" align="left">Department: {{$employee->dept_name  ?? ''}}</th>
+    <th colspan="{{count($leave_codes)*2}}" align="left">Department: {{$employee->dept_name  ?? ''}}</th>
 </tr>
 <tr>
     <th colspan="2"> Date</th>
@@ -41,10 +41,10 @@
     <tr>
     <td align="left">{{$start_time}}</td>
     <td align="left">{{$end_time }}</td>
-    <td align="center">{{$lrecord['leave_info']->leave_day ?? ''}}</td>
+    <td align="center">{{$lrecord['leave_info']->leave_day ?? ''}} ({{$lrecord['leave_code'] ?? ''}})</td>
     @foreach($leave_codes ?? [] as $leave)
         <td align="center">{{ $lrecord['leave_code']== $leave ?($lrecord['leave_info']->leave_day ?? ''):'' }}</td>
-        <td align="center">{{ $lrecord['leave_code']== $leave ?($lrecord['leave_balance'] ?? ''):'' }}</td>
+        <td align="center">{{ $lrecord['leave_code']== $leave ?($lrecord['leave_balance'] ?? ''):$employees_leave_balance[$employee->id.'___'.$leave]  }}</td>
     @endforeach
 </tr>
 @empty
